80e5b789095a5d904f14e599d84b55f61d5746a2
Glauco Pater <>
2021-01-01 14:03:42 +0100

    Update server scripts

--- scripts/batch.sh
+++ scripts/batch.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 pwd
-#wget https://covid.ourworldindata.org/data/owid-covid-data.json -P ./data/
-python3.4 scripts/server.py > ../mysite/result.json
+rm ./owid-covid-data/data/owid-covid-data.json.old
+mv ./owid-covid-data/data/owid-covid-data.json ./owid-covid-data/data/owid-covid-data.json.old
+wget -nv https://covid.ourworldindata.org/data/owid-covid-data.json -P ./owid-covid-data/data/
+python3.4 ./owid-covid-data/scripts/server.py > ./mysite/result.json
--- scripts/server.py
+++ scripts/server.py
@@ -3,18 +3,23 @@
 import jmespath
 import json
 
-filename = "./data/owid-covid-data.json"
+filename = "../data/owid-covid-data.json"
 
 with open(filename) as json_file:
     jsonContent = json.load(json_file)
 
-lastday = str(-2)
-query = (
-    "*.{location: location, date: data["
-    + lastday
-    + "].date, vaccinations: data["
-    + lastday
-    + "].total_vaccinations}"
-)
-res = jmespath.search(query, jsonContent)
-print(json.dumps(res))
+
+def getResult(lastDayInt):
+    lastday = str(lastDayInt)
+    query = (
+        "*.{location: location, date: data["
+        + lastday
+        + "].date, vaccinations: data["
+        + lastday
+        + "].total_vaccinations}"
+    )
+    result = jmespath.search(query, jsonContent)
+    return json.dumps(result)
+
+
+print(getResult(-1))
