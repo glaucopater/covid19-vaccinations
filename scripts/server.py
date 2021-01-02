40fddc5a4c7c97acbed7bea7b3c1455dc7fa8669
Glauco Pater <>
2021-01-02 12:33:19 +0100

    Update data query

--- scripts/server.py
+++ scripts/server.py
@@ -9,17 +9,10 @@ with open(filename) as json_file:
     jsonContent = json.load(json_file)
 
 
-def getResult(lastDayInt):
-    lastday = str(lastDayInt)
-    query = (
-        "*.{location: location, date: data["
-        + lastday
-        + "].date, vaccinations: data["
-        + lastday
-        + "].total_vaccinations}"
-    )
+def getResult():
+    query = "*.{location: location,population:population,previousDate:data[-2].date,date: data[-1].date, previousVaccinations: data[-2].total_vaccinations,vaccinations: data[-1].total_vaccinations}"
     result = jmespath.search(query, jsonContent)
     return json.dumps(result)
 
 
-print(getResult(-1))
+print(getResult())
