ecbec091b0e134c89005d7a19015adadf94ffd08
Glauco Pater <>
2021-01-02 12:33:52 +0100

    Update data query

--- scripts/server.js
+++ scripts/server.js
@@ -1,13 +1,6 @@
 var jmespath = require('jmespath');
 
-const data = require('../data/owid-covid-data.json');
-const query = "*.{
-location: location,
-    population: population,
-        previousDate: data[-2].date,
-            date: data[-1].date,
-                previousVaccinations: data[-2].total_vaccinations,
-                    vaccinations: data[-1].total_vaccinations
-    }";
+const data = require('./data/owid-covid-data.json');
+const query = "*.{location: location,total_vaccinations: data[?total_vaccinations!=null]}[?length(total_vaccinations)>\"0\"].{location: location, tv: length(total_vaccinations), total_vaccinations: total_vaccinations[-1].total_vaccinations}";
 const res = jmespath.search(data, query);
 console.log(res);
